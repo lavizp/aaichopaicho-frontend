63e8323765b9d48973e651a15961ea8caca992c7
Laviz-pand <pandeylaviz@gmail.com>
2023-10-25 11:04:11 +0545

    Installation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { lightMode } from "@/styles/theme/lightMode";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightMode}>
+      <CssBaseline enableColorScheme />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import { Button, Box } from "@mui/material";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>INIT</>;
+  return (
+    <>
+      <Button variant="contained">asd</Button>
+      <Box sx={{ display: "flex", gap: "12px" }}>
+        <Button variant="contained">asd</Button>
+        <Button variant="contained">asd</Button>
+      </Box>
+    </>
+  );
 }
